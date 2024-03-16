d59a949aa070a04551445d7ab5c4c58a9132698e
muzamil-mia <hussainmuzamil862@gmail.com>
2024-03-16 15:58:31 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,42 @@
+/*
+<div id="parent">
+  <div id="child1">
+    <h1>hello</h1>
+  </div>
+</div>
+*/
+
+// const parent = React.createElement("div", {id:"parent"},React.createElement("div",{id:"child"},React.createElement("h1",{"id":"heading1"},"i am h1 tag")));
+/*
+const parent = React.createElement("div",{id:"parent"},React.createElement("div",{id:"child1"},[React.createElement("h1",{id:""},"i am h1"),React.createElement("h2",{id:""},"i am h2")]))
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent)
+*/
+
+//nested structure
+/*
+<div id="parent">
+  <div id="child1">
+    <h1>hello</h1>
+  </div>
+  <div id="child1">
+    <h1>hello</h1>
+  </div>
+</div>
+*/
+
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", { id: "" }, "i am h1"),
+    React.createElement("h2", { id: "" }, "i am h2"),
+  ]),
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", { id: "" }, "i am h1"),
+    React.createElement("h2", { id: "" }, "i am h2"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
